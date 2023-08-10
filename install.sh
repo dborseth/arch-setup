4b090d120ad32ab553e9652bd5b5267df1c20d5d
Daniel B <danieljorgen@gmail.com>
2023-08-10 16:05:54 +0200

    Fix paths

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ echo -e "\nCreating EFI and root partitions"
 # https://wiki.archlinux.org/title/dm-crypt/Encrypting_an_entire_system#Simple_encrypted_root_with_TPM2_and_Secure_Boot
 sgdisk -n 0:0:+512MiB -t 0:ef00 -c 0:EFISYSTEM "$disk"
 sgdisk -n 0:0:0 -t 0:8304 -c 0:linux "$disk"
-partprobe -s "$disk"
+sgdisk -p "$disk"
   
 echo -e "\nEncrypting root"
 # Then we encrypt the root partition. This prompts for an encryption password
@@ -129,37 +129,35 @@ sed -i "s/^#\(no_NB.UTF-8\)/\1/" /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 arch-chroot /mnt hwclock --systohc --utc
 
-install -m755 -d /etc/cmdline.d
-install -m644 "$script_dir/etc/cmdline-boot.conf" /mnt/etc/cmdline.d/boot.conf 
-install -m644 "$script_dir/etc/cmdline-zram.conf" /mnt/etc/cmdline.d/zram.conf 
-install -m644 "$script_dir/etc/cmdline-btrfs.conf" /mnt/etc/cmdline.d/btrfs.conf 
-install -m644 "$script_dir/etc/cmdline-security.conf" /mnt/etc/cmdline.d/security.conf 
+install -vm755 -d /mnt/etc/cmdline.d
+install -vm644 "$script_dir/etc/cmdline-boot.conf" /mnt/etc/cmdline.d/boot.conf 
+install -vm644 "$script_dir/etc/cmdline-zswap.conf" /mnt/etc/cmdline.d/zswap.conf 
+install -vm644 "$script_dir/etc/cmdline-btrfs.conf" /mnt/etc/cmdline.d/btrfs.conf 
+install -vm644 "$script_dir/etc/cmdline-security.conf" /mnt/etc/cmdline.d/security.conf 
 
 # https://wiki.archlinux.org/title/Unified_kernel_image#kernel-install 
 # Use kernel-install to install UKI kernels to the esp, and mask the mkinitcpio
 # pacman hooks. Requires a pacman hook for kernel-install that is installed later.
-install -pm644 "$script_dir/etc/kernel-install.conf" /mnt/etc/kernel/install.conf
-install -m644 "$script_dir/etc/mkinitcpio-base.conf" /mnt/etc/mkinitcpio.conf.d/base.conf 
-ln -sf /dev/null /mnt/etc/pacman.d/hooks/60-mkinitcpio-remove.hook
-ln -sf /dev/null /mnt/etc/pacman.d/hooks/90-mkinitcpio-install.hook
+install -vpm644 "$script_dir/etc/kernel-install.conf" /mnt/etc/kernel/install.conf
+install -vm644 "$script_dir/etc/mkinitcpio-base.conf" /mnt/etc/mkinitcpio.conf.d/base.conf 
 
 # https://wiki.archlinux.org/title/Kernel_mode_setting#Early_KMS_start
 if echo "${gpu_vendors[@]}" | grep -q "\bnvidia\b"; then
   # https://wiki.archlinux.org/title/NVIDIA#DRM_kernel_mode_setting
-  install -m644 "$script_dir/etc/cmdline.d/nvidia.conf" /etc/cmdline.d/nvidia.conf
-  install -m644 "$script_dir/etc/mkinitcpio.conf.d/nvidia.conf" /etc/mkinitcpio.conf.d/nvidia.conf
+  install -vm644 "$script_dir/etc/cmdline-nvidia.conf" /mnt/etc/cmdline.d/nvidia.conf
+  install -vm644 "$script_dir/etc/mkinitcpio-nvidia.conf" /mnt/etc/mkinitcpio.conf.d/nvidia.conf
 fi
  
 if echo "${gpu_vendors[@]}" | grep -q "\bamd\b"; then
-  install -m644 "$script_dir/etc/mkinitcpio.conf.d/amd.conf" /etc/mkinitcpio.conf.d/amd.conf
+  install -vm644 "$script_dir/etc/mkinitcpio-amd.conf" /mnt/etc/mkinitcpio.conf.d/amd.conf
 fi
 
 if echo "${gpu_vendors[@]}" | grep -q "\bintel\b"; then
-  install -m644 "$script_dir/etc/mkinitcpio.conf.d/intel.conf" /etc/mkinitcpio.conf.d/intel.conf
+  install -vm644 "$script_dir/etc/mkinitcpio-intel.conf" /mnt/etc/mkinitcpio.conf.d/intel.conf
 fi
 
 bootctl --root /mnt install
-install -pm644 "$script_dir/etc/loader.conf" /efi/loader/loader.conf
+install -vpm644 "$script_dir/etc/loader.conf" /mnt/efi/loader/loader.conf
 
 # https://github.com/swsnr/dotfiles/blob/db42fe95fceeac68e4fbe489aed5e310f65b1ae7/arch/bootstrap-from-iso.bash#L131
 kernel_versions=(/mnt/usr/lib/modules/*)
@@ -167,13 +165,12 @@ kernel_version="${kernel_versions[0]##*/}"
 arch-chroot /mnt kernel-install add "${kernel_version}" \
    "/usr/lib/modules/${kernel_version}/vmlinuz"
 
-sbctl create-keys
-
-sbctl sign -s -o /usr/lib/fwupd/efi/fwupdx64.efi.signed /usr/lib/fwupd/efi/fwupdx64.efi
-sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-boot64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-
-sbctl verify
-
+arch-chroot /mnt bash -c "
+  sbctl create-keys
+  sbctl sign -s -o /usr/lib/fwupd/efi/fwupdx64.efi.signed /usr/lib/fwupd/efi/fwupdx64.efi
+  sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-boot64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+  sbctl verify
+"
 
 
 echo -e "\nCreating new user"
@@ -184,8 +181,8 @@ useradd -R /mnt -m -s /usr/bin/zsh "$username"
 usermod -R /mnt -aG wheel,storage,power,audit "$username"
 passwd -R /mnt "$username"
 
-install -dm750 /etc/sudoers.d/
-install -pm600 "$script_dir/etc/sudoers-wheel" /etc/sudoers.d/wheel
+install -vdm750 /mnt/etc/sudoers.d/
+install -vpm600 "$script_dir/etc/sudoers-wheel" /mnt/etc/sudoers.d/wheel
 
 # Set up dotfiles in home as a bare repository
 sudo -R /mnt -u "$username" bash -c 'git clone --bare https://github.com/dborseth/.dotfiles.git $HOME/.dotfiles'
@@ -210,10 +207,10 @@ aur_packages=(aurutils amdctl pacman-hook-kernel-install auto-cpufreq gtklock
 # Sets up a local aur repository and syncs the list of aur packages to the repo.
 # The packages are then installed along with the other packages in the pacman repo. 
 # TODO Move the repository to one of the servers to remove this step
-install -pm644 "$script_dir/etc/pacman.conf" /etc/pacman.conf
+install -vpm644 "$script_dir/etc/pacman.conf" /mnt/etc/pacman.conf
 
 sudo -R /mnt -u "$username" bash -c "git clone https://aur.archlinux.org/aurutils.git && cd aurutils && makepkg -si"
-install -d /mnt/var/cache/pacman/aur -o $username
+install -vd /mnt/var/cache/pacman/aur -o $username
 sudo -R /mnt -u "$username" bash -c "repo-add /var/cache/pacman/aur/aur.db.tar"
 sudo -R /mnt -u "$username" bash -c "aur sync ${aur_packages[@]}"
 
@@ -225,7 +222,7 @@ pacman -S --root /mnt --noconfirm "${extra_packages[@]}"
 # https://wiki.archlinux.org/title/NetworkManager#systemd-resolved
 # https://wiki.archlinux.org/title/NetworkManager#Using_iwd_as_the_Wi-Fi_backend
 ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
-install -Dpm644 "$script_dir/etc/networkmanager-wifi.conf" /mnt/etc/NetworkManager.conf.d/wifi.conf
+install -Dvpm644 "$script_dir/etc/networkmanager-wifi.conf" /mnt/etc/NetworkManager.conf.d/wifi.conf
 
 # TODO greetd
 
@@ -237,5 +234,5 @@ systemctl --root /mnt enable \
 
 echo -e '\n*** Installation script finished, cleaning up'
 
-umount -R /mnt
-cryptsetup luksClose root
+#umount -R /mnt
+#cryptsetup luksClose root
